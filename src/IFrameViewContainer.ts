b2aa6d3026b917f94f1ce7c8d04a4bae42437d4d
Alexander Krause <akr@informatik.uni-kiel.de>
2023-06-04 16:30:52 +0200

    remove logs

--- src/IFrameViewContainer.ts
+++ src/IFrameViewContainer.ts
@@ -28,7 +28,6 @@ export class IFrameViewContainer {
     this.refreshHTML();
 
     this.view.onDidReceiveMessage((data) => {
-      console.log("its beatiful");
       handleIncomingVizEvent(data);
     });
   }
