4532fa223835e916f4cd28eb84df39e5b5ca0ea8
Natalie Kaufhold <stu222510@mail.uni-kiel.de>
2024-02-21 10:54:08 +0100

    No CrossWindow while websocket is activated. However, CrossWindow seems to be deactivated all the time now.

--- media/cross-communication-webview.js
+++ media/cross-communication-webview.js
@@ -1,5 +1,8 @@
 //@ts-check
 
+import { currentMode } from '../src/extension';
+import { ModesEnum } from '../src/types';
+
 (function () {
   // @ts-ignore
   const vscode = acquireVsCodeApi();
@@ -10,19 +13,25 @@
       if (data.event) {
         let iframe = document.getElementById("explorviz-iframe");
 
-        if (iframe) {
+        if (iframe && currentMode === ModesEnum.crossWindow) {
           // @ts-ignore
           const iFrameWindow = iframe.contentWindow;
           // forward extension request to iframe
           iFrameWindow.postMessage(data.data, data.targetOrigin);
         }
       } else {
-        // forward iframe request to extension
-        forwardToExtension(data);
+        if (currentMode === ModesEnum.crossWindow) {
+          // forward iframe request to extension
+          forwardToExtension(data);
+        }
       }
     }
   });
 
+  /**
+   * Forward data from IFrame to extension.
+   * @param {*} data  Data, which shall be forwarded
+   */
   function forwardToExtension(data) {
     vscode.postMessage(data);
   }
